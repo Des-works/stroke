38955b9995b822dfe583fea40431d01d1132b68f
Destiny Edet <xdestiny247@gmail.com>
2022-05-20 16:52:20 +0100

    update

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ for(i = 0; i < log.length; i++) {
     console.log(`Letter ${i} is ${log[i].getTotalLength()}`);
 }
 // set timer that refreshes your page every 5s
+setTimeout(() => {
+    window.location.reload(true);
+}, 5000);
