5a7024fe79b7c61856f60338a807ec81a97c8caa
Destiny Edet <xdestiny247@gmail.com>
2022-05-20 16:25:21 +0100

    SVG stroke animetion

---
+++ app.js
@@ -0,0 +1,6 @@
+const log = document.querySelectorAll('#word #logo path');
+
+for(i = 0; i < log.length; i++) {
+    console.log(`Letter ${i} is ${log[i].getTotalLength()}`);
+}
+// set timer that refreshes your page every 5s
